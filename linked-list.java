be32bfb74d0a44bc7bf2abc26dfe6a6f3c36f1a1
Utkarsh Sinha <gitboyrex@gmail.com>
2024-02-13 23:24:17 +0530

    Data Structure

---
+++ linked-list.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class test{
+	public static void main(String[] args){
+		LinkedList<String> numbers = new LinkedList<String>();
+		numbers.add("1");
+		numbers.add("2");
+		numbers.add("3");
+		numbers.add("4");
+		numbers.add("5");
+		System.out.println(numbers);
+	}
+}
